8ea6b898fc8f013d34d0c70294c14b7d0f0c2c54
Roberto Gonzalez <rgonzalez7359@gmail.com>
2023-02-23 23:41:32 -0800

    default react installs plus bootsrap

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='App'>
+        <h1 class='page-header text-center'> React CRUD with PHP</h1>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
